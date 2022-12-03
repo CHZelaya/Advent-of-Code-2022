6c4c1b83943f284276d407b65eb83034f03d0ee9
CHZelaya <chzelaya1201@gmail.com>
2022-12-02 20:09:06 -0700

    Comitting latest progress, Was able to solve Part 1 of the puzzle

--- Dec-1/script.js
+++ Dec-1/script.js
@@ -1,17 +1,41 @@
-import { readFile } from 'fs';
+import { readFileSync } from 'fs';
 
-readFile('./puzzle-input', (err, data) => {
-    if (err) {
-        console.log('errrrrrorrrr')
+const puzzle = readFileSync('./puzzle-input.txt', 'utf8')
+let sortedInventory = puzzle.toString().split('\r\n');
+
+
+
+const checkInventory = (sortedInventory) => {
+    let arrayLength = sortedInventory.length;
+    let backPack = [];
+    let caloriesPerBag = 0;
+    let actualCalories = 0;
+
+    for (let i = 0; i < arrayLength; i++) {
+        actualCalories = Number(sortedInventory.pop());
+        caloriesPerBag = caloriesPerBag + actualCalories;
+        if (actualCalories === 0) {
+            backPack.push(caloriesPerBag);
+            caloriesPerBag = 0;
+        }
     }
-    console.log(data.toString('utf8'));
-})
+    caloriesPerBag = 0;
+    console.log(backPack)
+    backPack.sort((a, b) => a - b)
+    return console.log(backPack[backPack.length - 1]);
 
+
+
+}
+
+checkInventory(sortedInventory)
 
 // * Find the elf carrying the most calories.
 // * Spaces between the lines indicate a different elves inventory.
 
 //Todo: Create an app that can distinguish between the different inventories carried by the elves, sum their total and figure out which one is carrying the most energy.
 //Todo Step 1: Distinguish between the inventories.
-//Todo Step 2: Add the total calories per inventory.
-//Todo Step 3: Declare which elf is carrying the most energy.
+//Todo Step 2: Add the total calories per inventory, into a new array.
+//Todo ForEach array add all values and print to new array
+//Todo Step 3: Iterate through new arrays and find biggest number value, via if statement
+
